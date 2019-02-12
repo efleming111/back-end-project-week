339847a83439a2e11e852aa4a54828cf4710714e
Eric Fleming <efleming111@yahoo.com>
2019-02-12 06:00:55 -0700

    Added get and post endpoints

--- index.js
+++ index.js
@@ -10,15 +10,47 @@ server.use(express.json());
 const PORT = 3000;
 
 server.get('/api/notes', (req, res)=>{
-    res.status(200).json({message: 'Return all notes'});
+    db('notes')
+    .then(notes=>{
+        res.status(200).json({notes: notes});
+    })
+    .catch(error=>{
+        res.status(500).json({error: 'Failed to retrieve notes'});
+    })
 })
 
 server.get('/api/notes/:id', (req, res)=>{
-    res.status(200).json({message: 'Return note by id'});
+    const {id} = req.params;
+    db('notes')
+    .where('id', Number(id))
+    .then(note=>{
+        if(note.length){
+            res.status(200).json({note: note});
+        }
+        else{
+            res.status(404).json({errorMessage: 'Note not found'});
+        }
+    })
+    .catch(error=>{
+        res.status(500).json({error: 'Failed to retrieve note'});
+    })
 })
 
 server.post('/api/notes', (req, res)=>{
-    res.status(200).json({message: 'Add note return id'})
+    const note = req.body;
+    if(note.note_title){
+        db('notes')
+        .insert(note)
+        .then(id=>{
+            res.status(201).json({id: id[0]});
+        })
+        .catch(error=>{
+            res.status(500).json({error: 'Failed to add note to database'});
+        })
+    }
+    else{
+        res.status(400).json({errorMessage: 'Please include the title of the note'});
+    }
 })
 
 server.put('/api/notes/:id', (req, res)=>{
